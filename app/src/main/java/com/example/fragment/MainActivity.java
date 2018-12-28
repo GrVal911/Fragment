75341b43bc6a387689fc1fe8c160e97532bc91f9
Valentin <grvalya911@mail.ru>
2018-12-28 14:18:41 +0300

    changing color of background in different commits

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.fragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void change (View view){
+        Fragment fragment = null;
+        switch(view.getId()){
+            case R.id.button:
+                fragment =new Fragment1();
+                break;
+            case R.id.button2:
+                fragment =new Fragment2();
+                break;
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fr_place, fragment);
+        ft.commit();
     }
 }
